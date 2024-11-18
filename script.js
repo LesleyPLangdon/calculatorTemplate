e597ad10683cbb239e2afa2db0acbc99b1f20bf3
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-11-18 11:54:55 -0500

    added function stubs

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+/********************** addition function  *********************/
+add(num1, num2) {
+
+}
+
+/********************** subtraction function  *********************/
+subtract(num1, num2) {
+
+}
+
+/********************** multiplication function  *********************/
+multiply(num1, num2) {
+
+}
+
+/********************** division function  *********************/
+divide(num1, num2) {
+
+}
+
+/********************** clear function  *********************/
+clear(num1, num2) {
+
+}
+
+/********************** calculate function  *********************/
+calculate(num1, num2) {
+
+}
