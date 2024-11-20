e99683a66f70b6bf19293a4ca4b46b7031b3d7ce
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-11-20 12:41:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
 /********************** addition function  *********************/
-add(num1, num2) {
+function add(num1, num2) {
 
 }
 
 /********************** subtraction function  *********************/
-subtract(num1, num2) {
+function subtract(num1, num2) {
 
 }
 
 /********************** multiplication function  *********************/
-multiply(num1, num2) {
+function multiply(num1, num2) {
 
 }
 
 /********************** division function  *********************/
-divide(num1, num2) {
+function divide(num1, num2) {
 
 }
 
 /********************** clear function  *********************/
-clear(num1, num2) {
+function clear(num1, num2) {
 
 }
 
 /********************** calculate function  *********************/
-calculate(num1, num2) {
+function calculate(num1, num2) {
 
 }
